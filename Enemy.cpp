39e048db05aa3690fed0a70ff1743562bef28379
Brody <brodywilton@hotmail.com>
2025-01-03 16:22:36 +0000

    Implement basic enemy AI, it chases the knight

--- Enemy.cpp
+++ Enemy.cpp
@@ -9,13 +9,24 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
     run = run_texture;
     width = texture.width / maxFrames;
     height = texture.height;
+    speed = 3.5f;
 }
 
 void Enemy::tick(float deltaTime)
 {
+    // Get toTarget vector
+    Vector2 toTarget = Vector2Subtract(current_target->getScreenPos(), screenPos);
+    // Normalise the vector
+    toTarget = Vector2Normalize(toTarget);
+    // multiply it by speed
+    toTarget = Vector2Scale(toTarget, speed);
+    // move the enemy (set worldPos)
+    worldPos = Vector2Add(worldPos, toTarget);
+
     screenPos = Vector2Subtract(worldPos, current_target->getWorldPos());
 
     BaseCharacter::tick(deltaTime);
+
 
 }
 
